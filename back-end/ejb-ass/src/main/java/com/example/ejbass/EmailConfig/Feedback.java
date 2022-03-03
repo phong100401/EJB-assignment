dcf94e92bb1e7263051fe6f7890ac45277b593aa
duckun2512 <65001215+duckun2512@users.noreply.github.com>
2022-03-03 10:38:34 -0500

    update email sender test

---
+++ back-end/ejb-ass/src/main/java/com/example/ejbass/EmailConfig/Feedback.java
@@ -0,0 +1,12 @@
+package com.example.ejbasmtest.EmailConfig;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Feedback {
+    private String name;
+    private String email;
+    private String feedback;
+}
